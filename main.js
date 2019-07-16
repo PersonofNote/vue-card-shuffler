73a1c2fe17d3d6df876f20d002602e14f04eb10b
Jessie <jmartin@matrixgroup.net>
2019-07-16 14:25:32 -0400

    Initialize methods (note taking)

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 //First, create Vue instance and initialize three properties
 new Vue({
   el: '#app',
+  //Note for self later. Need an extra suit, extra ranks, and a whole
+  //extra set of ranks for the Major Arcana to shuffle in.
+  //Add variables/constants
+  data: {
+    ranks: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+    suits: ['♥','♦','♠','♣'],
+    cards: [],
+  },
+  //Add actions
+  methods: {
+  displayInitialDeck() {/*Initialize id at 1, initialize value as [], loop through.*/},
+  }
 });
