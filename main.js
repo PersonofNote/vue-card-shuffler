6db400213986062a7a59a895ab4f8364633f2644
Jessie <jmartin@matrixgroup.net>
2019-07-16 15:20:58 -0400

    Lots of notes about vue being a virtual representation of a physical card deck

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 new Vue({
+    /*In this instance, the new Vue is like the deck. This helps a lot with visualization.
+    * So you have your deck, and :
+    * -el is where you put your deck
+    * -data is how you'd describe the cards in your deck.
+    * -created is how you first set out your deck
+    * -methods are everything you do with the deck
+    * -this is... this deck.
+    * */
     el: '#app',
     data: {
       ranks: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
@@ -37,10 +45,13 @@ new Vue({
         for(let i = this.cards.length - 1; i > 0; i--) {
             //get random
             let randomIndex = Math.floor(Math.random() * i);
-            //swap cards[i] with cards[random]
+            //swap cards[i] with cards[random].
+            //Fischer Yates isn't very random because random() isn't very random, 
+            //consider refactoring at the end.
             let temp = this.cards[i];
-            this.cards[i] = this.cards[randomIndex];
-            this.cards[randomIndex] = temp;
+            //Change to Vue.set because Vue doesn't pick up changes when array values/length are directly set.
+            Vue.set(this.cards, i, this.cards[randomIndex]);
+            Vue.set(this.cards, randomIndex, temp);
           }
       },
     },
